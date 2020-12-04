28cfea0a04ba1153d8ef76d204821e2b7e6f8ac2
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-12-03 22:51:05 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,8 +6,13 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+})
 
-let Person;
+const Person = mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
